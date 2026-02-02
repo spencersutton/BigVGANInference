0599876d336e406a6a9b5858e9b5dc462dab4fd9
Spencer Sutton <spencer.e.sutton@gmail.com>
2026-02-01 23:33:54 -0500

    remove version metadata import

--- bigvganinference/__init__.py
+++ bigvganinference/__init__.py
@@ -1,7 +1,3 @@
-import importlib.metadata
-
 from BigVGANInference.bigvganinference.inference import BigVGANHFModel, BigVGANInference
-
-__version__ = importlib.metadata.version("bigvganinference")
 
 __all__ = ["BigVGANInference", "BigVGANHFModel"]
